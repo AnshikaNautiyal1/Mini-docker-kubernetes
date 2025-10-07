f0763da5758b1e4e42637376220bc1dc59f542fd
AnshikaNautiyal1 <158483122+AnshikaNautiyal1@users.noreply.github.com>
2025-10-07 23:14:33 +0530

    Update master.py

---
+++ master/master.py
@@ -0,0 +1,17 @@
+import socket, json
+
+def send_command(node_ip, command):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((node_ip, 5000))
+    s.send(command.encode())
+    data = s.recv(1024).decode()
+    print("Response:", data)
+    s.close()
+
+if __name__ == "__main__":
+    while True:
+        cmd = input("EduDistOS> ")
+        if cmd.startswith("run") or cmd.startswith("list") or cmd.startswith("stop"):
+            send_command("127.0.0.1", cmd)
+        else:
+            print("Unknown command")
